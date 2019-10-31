3b8195776c5bbf27d17966c774bd5425b4cbc48c
guillermo saavedra <gs8rj@virginia.edu>
2019-10-30 23:50:57 -0400

    Fixes path issues and downloads file when URL is accessed

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,18 @@
-import express from 'express'
-const walk = require('../traversing.js');
+const express = require('express');
 const ngrok = require('ngrok');
+const path = require('path');
 const PORT = 3000;
 
 const app = express();
 
 export function cli(args) {
-  walk(args[2], function(err, results) {
-    console.log(results); 
-  }, app);
-  app.use(args[2].substr(1), express.static(args[2].substr(2)))
   app.listen(3000, async function(req, res) {
     const url = await ngrok.connect(PORT);
-    console.log('Listening....');
-	console.log(url);
+    console.log("Your file "+args[2]+" is available at: "+url);
   });
 
-}
+  app.get('/', function(req, res) {
+    console.log("Your file "+args[2]+" has been downloaded");
+    res.download(path.join(process.cwd(), args[2]));
+  })
+};
