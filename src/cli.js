df036dee0fee1da180e4dde98ab0565c1aad2f9d
guillermo saavedra <gs8rj@virginia.edu>
2020-05-03 23:25:20 -0400

    Adds color to log output

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ngrok = require('ngrok');
+const colors = require('colors');
 const path = require('path');
 const fs = require('fs')
 const PORT = 6000;
@@ -9,16 +10,15 @@ const app = express();
 export function cli(args) {
   const filePath = args[2];
   if (!fs.existsSync(filePath)) {
-    console.log("The file does not exist. Please try again (:");
+    console.log("ERROR: The file path not exist. Please try again.".brightRed);
   } else {
     app.listen(PORT, async function(req, res) {
       const url = await ngrok.connect(PORT);
-      console.log("Your file "+filePath+" is available at: "+url);
-      console.log("Share the URL with anyone across the web!")
+      console.log("Your file "+filePath.brightGreen+" is available at -> "+url.brightBlue);
     });
 
     app.get('/', function(req, res) {
-      console.log("Your file "+filePath+" has been downloaded by someone");
+      console.log("Your file "+fileName+" has been downloaded.");
       res.download(path.join(process.cwd(), filePath));
     })
   }
