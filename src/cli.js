225715c990e385aa39ebae46c135b6b4b88080a8
guillermo saavedra <gs8rj@virginia.edu>
2020-05-04 22:47:02 -0400

    Adds more informative log output

--- src/cli.js
+++ src/cli.js
@@ -14,12 +14,14 @@ export function cli(args) {
   } else {
     const server = app.listen(PORT, async function(req, res) {
       const url = await ngrok.connect(server.address().port);
-      console.log("SUCCESS: ".brightGreen)
-      console.log(`Your file ${filePath.brightGreen} is available for download at -> ${url.brightBlue}`);
+      console.log('SUCCESS! '.brightGreen)
+      console.log(`Your file ${filePath.yellow} is available for download at -> ${url.brightBlue}`);
+      console.log('We will notify you here once the file has been downloaded.')
     });
 
     app.get('/', function(req, res) {
-      console.log(`Your file ${filePath} has been downloaded.`);
+      console.log(`Your file ${filePath} has been downloaded! 'CTRL + C' to stop sharing.`.brightYellow);
       res.download(path.join(process.cwd(), filePath));
-    })
-  } };
+    });
+  };
+};
