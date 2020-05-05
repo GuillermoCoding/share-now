9f4e18a3fdfb5e76ae4572d8ae3440d172789475
guillermo saavedra <gs8rj@virginia.edu>
2020-05-04 22:26:31 -0400

    Changes PORT to 0 to find available port

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ const ngrok = require('ngrok');
 const colors = require('colors');
 const path = require('path');
 const fs = require('fs')
-const PORT = 6000;
+const PORT = 0;
 
 const app = express();
 
@@ -12,14 +12,14 @@ export function cli(args) {
   if (!fs.existsSync(filePath)) {
     console.log(`ERROR: The file path not exist. Please try again.`.brightRed);
   } else {
-    app.listen(PORT, async function(req, res) {
-      const url = await ngrok.connect(PORT);
-      console.log(`Your file ${filePath.brightGreen} is available at -> ${url.brightBlue}`);
+    const server = app.listen(PORT, async function(req, res) {
+      const url = await ngrok.connect(server.address().port);
+      console.log("SUCCESS: ".brightGreen)
+      console.log(`Your file ${filePath.brightGreen} is available for download at -> ${url.brightBlue}`);
     });
 
     app.get('/', function(req, res) {
-      console.log(`Your file ${fileName} has been downloaded.`);
+      console.log(`Your file ${filePath} has been downloaded.`);
       res.download(path.join(process.cwd(), filePath));
     })
-  }
-};
+  } };
