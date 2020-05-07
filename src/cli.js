bf629e915a5ef45a040d59308b7a0272a3071d5a
guillermo saavedra <gs8rj@virginia.edu>
2020-05-06 21:58:36 -0400

    Adds user-agent parser

--- src/cli.js
+++ src/cli.js
@@ -1,27 +1,38 @@
-const express = require('express');
-const ngrok = require('ngrok');
 const colors = require('colors');
-const path = require('path');
+const express = require('express');
 const fs = require('fs')
-const PORT = 0;
+const ngrok = require('ngrok');
+const path = require('path');
+const expressUserAgent = require('express-useragent');
 
+const PORT = 0;
 const app = express();
 
 export function cli(args) {
   const filePath = args[2];
+
   if (!fs.existsSync(filePath)) {
     console.log(`ERROR: The file path not exist. Please try again.`.brightRed);
   } else {
-    const server = app.listen(PORT, async function(req, res) {
+    const server = app.listen(PORT, async (req, res) => {
       const url = await ngrok.connect(server.address().port);
-      console.log('SUCCESS! '.brightGreen)
+      console.log('SUCCESS! '.brightGreen);
       console.log(`Your file ${filePath.yellow} is available for download at -> ${url.brightBlue}`);
-      console.log('We will notify you here once the file has been downloaded.')
+      console.log('We will notify you here once the file has been downloaded.');
     });
+    app.get('/', (req, res) => {
+      const source = req.headers['user-agent'];
+      const userAgent = expressUserAgent.parse(source);
 
-    app.get('/', function(req, res) {
-      console.log(`Your file ${filePath} has been downloaded! 'CTRL + C' to stop sharing.`.brightYellow);
-      res.download(path.join(process.cwd(), filePath));
+      if (!userAgent.isBot) {
+        res.download(path.join(process.cwd(), filePath), (err)=>{
+          if (err) {
+            console.log(`ERROR: The file was not able to be downloaded successfully. Please try again`.brightRed);
+          } else {
+            console.log(`Your file ${filePath} has been downloaded! 'CTRL + C' to stop sharing.`.brightYellow);
+          }
+        });
+      }
     });
   };
 };
